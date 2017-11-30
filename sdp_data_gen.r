424b288a4759a05563c14583bd03ae8b53e08685
Narnian12 <pitaah@umich.edu>
2017-11-30 13:12:49 -0500

    Fixed matrix nrow to fit different numbers of samples

--- sdp_data_gen.r
+++ sdp_data_gen.r
@@ -1,7 +1,7 @@
 sdp_data_gen <- function(ind_sample_grep, sample_ordering, otu.good.norm_in, otu.good.taxonomy_in) {
   ordering <- 50
   incr <- 0
-  stacked_data <- matrix(nrow = 250, ncol = 2)
+  stacked_data <- matrix(nrow = (50 * length(ind_sample_grep)), ncol = 2)
   for (i in 1:50) {
     for (j in 1:length(ind_sample_grep)) {
       stacked_data[i + incr, 1] <- OtuConvert(sample_ordering[i], ordering, as.character(otu.good.taxonomy_in[sample_ordering, 6][i]))

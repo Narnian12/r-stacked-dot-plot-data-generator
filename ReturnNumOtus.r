67eeaceb52624017ffd477acdd1d91b72c622654
Narnian12 <pitaah@umich.edu>
2017-12-07 14:31:41 -0500

    Added RME

--- ReturnNumOtus.r
+++ ReturnNumOtus.r
@@ -1,4 +1,12 @@
-
+# REQUIRES: otuVec which is as.numeric(otu.good.norm[sample_id, order]) where:
+  # sample_id = the specific index of the sample within otu.good.norm to be analyzed
+  # order = the specific sample's order
+  # e.g. if my specific sample is balb_2l_feces_d0 and its order is balb_2l_feces_d0.order,
+    # the variable would be <as.numeric(otu.good.norm[balb_2l_feces_d0, balb_2l_feces_d0.order])
+  # NOTE - ReturnNumOtus() is ONLY used within GenerateNumOtus(), so you will not need to worry
+    # about sample_id and order
+# MODIFIES: None
+# EFFECTS: Returns the number of OTUs within the specific sample that has a higher relative abundance than 0.01%
 ReturnNumOtus <- function(otuVec) {
   numOtus <- 0
   for (k_in in 1:length(otuVec)) {

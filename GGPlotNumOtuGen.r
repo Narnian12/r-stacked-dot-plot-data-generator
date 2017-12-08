61c35e01bb3993c2aeaefad4134e949f12685c53
Narnian12 <pitaah@umich.edu>
2017-12-08 10:25:09 -0500

    Added ggplot generator for GenerateNumOtus()

---
+++ GGPlotNumOtuGen.r
@@ -0,0 +1,8 @@
+# REQUIRES: The following variables - 
+	# num_otus_vec_in which is the result of SDPDataGen()
+	# ggtitle_in which is what you want the ggplot title to be (must be within quotations, eg "Cecum GGPlot Analysis for Day 0")
+# MODIFIES: None
+# EFFECTS: Returns ggplot of stacked dot plots for number of OTUs per sample per day (based on result of SDPDataGen())
+GGPlotGen <- function(num_otus_vec_in, ggtitle_in) {
+  ggplot(num_otus_vec_in, aes(x = Day, y = RelAbund)) + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + geom_count(col = "black", show.legend = F, shape = 21, size = 3) + ggtitle(ggtitle_in) + theme(plot.title = element_text(hjust = 0.5)) + ylab("# of OTUs") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+}

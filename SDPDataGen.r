0aef4537358af5b5a64b95fe9508bd909e0101c8
Narnian12 <pitaah@umich.edu>
2017-12-08 11:31:54 -0500

    Added to RME

--- SDPDataGen.r
+++ SDPDataGen.r
@@ -3,6 +3,7 @@
   # sample_ordering that contains the combined OTU ordering of all the individual samples (eg if there were three samples in feces_d0, then the sample_ordering <- feces_d0.order)
   # otu.good.norm_in which is otu.good.norm
   # otu.good.taxonomy_in which is otu.good.taxonomy
+  # OtuConvert() which is an Rcpp function given in the default .R file within GitHub
 # MODIFIES: None
 # EFFECTS: Returns stacked_data which is a 2-D matrix with the first column containing the most-to-least abundant OTUs (up to the 50th most abundant)
   # and the second column containing the % relative abundances
